9153da506b8da8579917286e8246736a2329e2a5
shawnkim999 <shawnkimprojects@gmail.com>
2025-08-31 22:44:21 -0700

    Deleting files

--- API/src/routes/postRoutes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { getAllPosts } from "../controllers/postController";
-
-const router = Router();
-
-router.use("/", getAllPosts);
-
-export default router;
